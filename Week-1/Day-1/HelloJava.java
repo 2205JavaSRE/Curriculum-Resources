c9d0be415ccfd7afef817da2e00f5387ddf92757
BenArayathel <ben.arayathel@gmail.com>
2022-05-09 16:17:26 -0400

    First examples and notes

---
+++ Week-1/Day-1/HelloJava.java
@@ -0,0 +1,13 @@
+//This is a comment line, Java will just ignore this!
+//Java has a class structure
+//Everthything has to be wrapped in a class
+// Also need a Main method, to have an entry point. 
+
+public class HelloJava{
+	public static void main(String[] args){
+		System.out.println(args[0]);
+		
+
+	}
+
+}
